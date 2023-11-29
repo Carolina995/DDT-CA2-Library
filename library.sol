ac4e8489096da5c0ca6060dbc018dd13524d8157
Carolina Gomes Landim <83533485+Carolina995@users.noreply.github.com>
2023-11-29 00:58:02 +0000

    Borrow book function

--- library.sol
+++ library.sol
@@ -18,3 +18,8 @@ function addBook(uint _id, string memory _title) public {
         require(msg.sender == librarian, "Only the librarian can add books.");
         books[_id] = Book(_title, address(0));
     }
+
+ function borrowBook(uint _id) public {
+        require(books[_id].currentHolder == address(0), "This book is currently borrowed by someone else.");
+        books[_id].currentHolder = msg.sender;
+    }
